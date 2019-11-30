2c844ecc0a2f5bbe909f651ae5cf03606e20de3b
Th3Shadowbroker <webmaster@m4taiori.de>
2019-11-30 16:42:28 +0100

    Added support for output-hoppers

--- src/org/inventivetalent/bookshelves/HopperListener.java
+++ src/org/inventivetalent/bookshelves/HopperListener.java
@@ -5,10 +5,8 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Hopper;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.inventory.BlockInventoryHolder;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
@@ -43,13 +41,32 @@ public class HopperListener implements Listener {
             // Perform safe item transfer from hopper to shelf
             facedBlockInventory.ifPresent(
                     shelf -> {
+                        Optional<Hopper> outputHopper = getOutputHopper(facedBlock);
+                        System.out.println(facedBlock.getType().name());
+
                         event.getSource().remove(event.getItem());
-                        boolean success = shelf.addItem(event.getItem()).isEmpty();
+
+                        // Used to prevent items from vanishing/duplicating
+                        boolean success;
+
+                        // Check if the shelf acts as conveyor
+                        if (!outputHopper.isPresent()) {
+                            success = shelf.addItem(event.getItem()).isEmpty();
+                        } else {
+                            // Push to connected output-hopper or the shelf if the output-hopper is full/locked
+                            boolean hopperCanReceive = !outputHopper.get().isLocked() && outputHopper.get().getInventory().firstEmpty() > -1;
+                            success = hopperCanReceive ? outputHopper.get().getInventory().addItem(event.getItem()).isEmpty() : shelf.addItem(event.getItem()).isEmpty();
+                        }
 
                         event.setCancelled(success);
                     }
             );
         }
+    }
+
+    public Optional<Hopper> getOutputHopper(Block block) {
+        Block relative = block.getRelative(BlockFace.DOWN);
+        return relative.getType() == Material.HOPPER ? Optional.of((Hopper) relative.getState()) : Optional.empty();
     }
 
     private Block getFacedBlock(Hopper hopper) {
